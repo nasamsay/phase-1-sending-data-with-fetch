d499a28af7e9f762301ed89a1f2b18c0a7518cfa
Nasam Sayour <nasamsay@gmail.com>
2022-09-13 10:03:12 +0300

    Assignment Complete

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Add your code here
+function submitData(userName, userEmail) {
+    fetch('http://localhost:3000/users', {
+        method : 'POST',
+        headers : {
+            "Content-Type": "application/json",
+            Accept: "application/json",
+        },
+        body : JSON.stringify ( {
+            name : userName,
+            email : userEmail
+        })
+    } )
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (object) {
+        console.log(object);
+    })
+    .catch(function (error) {
+        alert("Bad things! Ragnarők!");
+        console.log(error.message);
+    });
+}
